628b85e8ccbd86ecd6e35698d5418b8735206cf5
usuario <usuario@T20424.ds.iteso.mx>
2014-04-23 10:27:29 -0500

    new commit

---
+++ src/gittest/Main.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gittest;
+
+/**
+ *
+ * @author usuario
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome to program\n");
+        new GUI().setVisible(true);
+    }
+}
